eb53974cf6351f166e5b89062c5da254f4e181dd
323 <323@323-12>
2022-09-16 12:11:26 +0300

    Заполнение сервисов

--- Laba111/Model/House.cs
+++ Laba111/Model/House.cs
@@ -8,5 +8,10 @@ namespace Laba111.Model
 {
     internal class House
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
     }
 }
---
+++ Laba111/Services/HouseService.cs
@@ -0,0 +1,39 @@
+﻿using Java.Util;
+using Laba111.Model;
+using Laba111.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static Android.Icu.Text.CaseMap;
+
+namespace Laba111.Services
+{
+    internal class HouseService
+    {
+        // Создаем список для хранения данных из источника
+        List<HouseService> HouseList = new();
+
+        // Метод GetHouse() служит для извлечения и сруктурирования данных
+        // в соответсвии с существующей моделью данных
+        public async Task<IEnumerable<HouseService>> GetHouse()
+        {
+            // Если список содержит какие-то элементы
+            // то вернется последовательность с содержимым этого списка
+            if (HouseList?.Count > 0)
+                return HouseList;
+
+            // В данном блоке кода осуществляется подключение, чтение
+            // и дессериализация файла - источника данных
+            using var stream = await FileSystem.OpenAppPackageFileAsync("House.json");
+            using var reader = new StreamReader(stream);
+            var contents = await reader.ReadToEndAsync();
+            HouseList = JsonSerializer.Deserialize<List<HouseService>>(contents);
+
+            return HouseList;
+        }
+    }
+
+}
